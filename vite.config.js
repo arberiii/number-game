66be405de0dd243350311efdd3509fbe4c07a4de
Arber Avdullahu <arbr.avdullahu@gmail.com>
2024-06-11 09:21:27 +0200

    Add vercel plugin

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { version } from "./package.json";
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/number-game/",
   plugins: [svelte({
     preprocess: vitePreprocess()
-  })],
+  }), vercel()],
+  vercel: {},
   // server: {
   //   port: 3006,
   // },
